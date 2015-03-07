b71f1633de9a9781d7ffcfcb363881df8a1ec522
Daniel Olano <danielolanoc@gmail.com>
2015-03-07 18:12:43 -0500

    polish REST end-points

--- server/models/category.js
+++ server/models/category.js
@@ -1,3 +1,8 @@
 module.exports = function(Category) {
-
+    // allow to only fetch
+    Category.disableRemoteMethod('create', true);
+    Category.disableRemoteMethod('deleteById', true);
+    Category.disableRemoteMethod('upsert', true);
+    Category.disableRemoteMethod('updateAll', true);
+    Category.disableRemoteMethod('updateAttributes', false);
 };
